83ad575a5a5345deddadc1851b4963ca2a99f8f0
Hilal <code.hilal@gmail.com>
2024-04-03 05:45:36 +0200

    responsive index

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-window.onscroll = function() {stickyNavbar()};
+window.onscroll = function () { stickyNavbar() };
 
 function stickyNavbar() {
   var navbar = document.getElementById("navbar");
-  if (window.pageYOffset >= 400) {
+  if (window.scrollY >= 400) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
@@ -11,4 +11,4 @@ function stickyNavbar() {
 
 
 const API_KEY = "fcb65153a7674120b954ad114b80187d";
-const URL = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=fcb65153a7674120b954ad114b80187d"
+const url = "https://newsapi.org/v2/everything?q= bitcoin"
